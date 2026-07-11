b653b7beb001533cf944f9634d5e36377ba7e62e
Edprime9 <morrisoneli9@gmail.com>
2026-07-12 00:06:09 +0100

    Add Post, Put, and Delete routes and used created a REST client (file) for testing my routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -41,7 +41,94 @@ const getSingle = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({
+        id: response.insertedId
+      });
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updateContact = async (req, res) => {
+
+  try {
+
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .replaceOne(
+        { _id: contactId },
+        contact
+      );
+
+    if (response.modifiedCount > 0) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const deleteContact = async (req, res) => {
+
+  try {
+
+    const contactId = new ObjectId(req.params.id);
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .deleteOne({
+        _id: contactId
+      });
+
+    if (response.deletedCount > 0) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
